1cac020a221bdd3e05db91eafd1aa5174240b45c
Aekyung <162764032+ak0896@users.noreply.github.com>
2024-03-10 13:49:25 +0900

    Update 소수 구하기.py

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,18 @@
 import math
+# math.row(2,4)   #2의4승=16   ->제곱함수
+# math.sqrt(4)    #루트4=2     ->제곱근함수
+
 
 m,n=map(int,input().split())
 
 def is_prime(x):
-  if x==1:
+  if x==1:             
     return False
-  for i in range(2,int(math.sqrt(x))+1):
-    if x%i==0:
+  for i in range(2,int(math.sqrt(x))+1):     
+    if x%i==0:                              ##반만 확인하면 나머지는 약수이므로
       return False
   else : return True
+
 
 for x in range(m,n+1):
   if (is_prime(x)==True):
